64443f5a84fdc356216b5460d34d6d53ec7d12c6
ECarlson <ericcarlson4@outlook.com>
2019-07-30 01:05:41 -0700

    initialized and tried to set up file

---
+++ client/my-app/src/Components/Navbar/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar () {
+  return (
+    <nav>
+      <h1>Google Books Search!</h1>
+      <h4>For Search</h4>
+      <h4>For Saved</h4>
+    </nav>
+  )
+}
+
+export default Navbar
